2c1cb21acc17f496f247b09ddf0f857a705269cf
Zeriab Chiah <zeriab@hotmail.com>
2022-07-05 00:32:43 +0300

    align

--- src/main/java/com/sys/sales/model/Product.java
+++ src/main/java/com/sys/sales/model/Product.java
@@ -9,74 +9,79 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.DynamicUpdate;
 
-@Table(name="Product") 
+@Table(name = "Product")
 @Entity
 @DynamicUpdate
 public class Product {
-	  @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    private Long id;
-	   
-	    	
-	    private String name;
-	 
-	    @Column(name="description",nullable = false, length = 200)
-	    private String description;
-	    
-	    @Column(name="category",nullable = false)
-	    private String category;
-	    
-	    @Column(name="creationDate",nullable = false)
-	    private String creationDate;
-	    
-	    @Column(name="price",nullable = false)
-	    private Long price;
-	    
-	    public void setId(Long id) {
-	    	this.id=id;
-	    }
-	    
-	    public long getId() {
-	    	return this.id;
-	    }
-	    public void setName(String name) {
-	    	this.name=name;
-	    }
-	    
-	    public String getName() {
-	    	return this.name;
-	    }
-	    public void setId(String description) {
-	    	this.description=description;
-	    }
-	    
-	    public String getDescription() {
-	    	return this.description;
-	    }
-	    public void setDescription(String description) {
-	    	this.description=description;
-	    }
-	    public void setCategory(String category) {
-	    	this.category=category;
-	    }
-	    
-	    public String getCategory() {
-	    	return this.category;
-	    }
-	    public void setPrice(Long price) {
-	    	this.price=price;
-	    }
-	    
-	    public Long getPrice() {
-	    	return this.price;
-	    }
-	    public void setCreationDate(String creationDate) {
-	    	this.creationDate=creationDate;
-	    }
-	    
-	    public String getCreationDate() {
-	    	return this.creationDate;
-	    }
-	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false, length = 200)
+	private String name;
+
+	@Column(name = "description", nullable = false, length = 200)
+	private String description;
+
+	@Column(name = "category", nullable = false)
+	private String category;
+
+	@Column(name = "creationDate", nullable = false)
+	private String creationDate;
+
+	@Column(name = "price", nullable = false)
+	private Long price;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public long getId() {
+		return this.id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setId(String description) {
+		this.description = description;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getCategory() {
+		return this.category;
+	}
+
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+
+	public Long getPrice() {
+		return this.price;
+	}
+
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public String getCreationDate() {
+		return this.creationDate;
+	}
+
 }
--- src/main/java/com/sys/sales/model/ProductsQuantities.java
+++ src/main/java/com/sys/sales/model/ProductsQuantities.java
@@ -1,49 +1,39 @@
 package com.sys.sales.model;
 
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.DynamicUpdate;
-
-
 public class ProductsQuantities {
 
-	    private Product product;
-	   
-	    private int quantity;
-	    
-public ProductsQuantities()
-{
-	this.quantity=0;
-}
+	private Product product;
 
-public int getQuantity() {
-    return this.quantity;
-}
+	private int quantity;
 
-public void setQuantity(int quantity) {
-    this.quantity = quantity;
-}
-	
-		/**
-		 * @return the product
-		 */
-		public Product getProduct() {
-			return product;
-		}
-		public double getAmount() {
-	        return this.product.getPrice() * this.quantity;
-	    }
-		/**
-		 * @param product the product to set
-		 */
-		public void setProduct(Product product) {
-			this.product = product;
-		}
-	
-	
+	public ProductsQuantities() {
+		this.quantity = 0;
+	}
+
+	public int getQuantity() {
+		return this.quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	/**
+	 * @return the product
+	 */
+	public Product getProduct() {
+		return product;
+	}
+
+	public double getAmount() {
+		return this.product.getPrice() * this.quantity;
+	}
+
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
 }
