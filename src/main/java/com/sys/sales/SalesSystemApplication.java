be7230f99f9a5dfa589a4128e89b4cf7d6cfc1ca
Zeriab Chiah <zeriab@hotmail.com>
2022-07-04 23:48:34 +0300

    removing log

--- src/main/java/com/sys/sales/SalesSystemApplication.java
+++ src/main/java/com/sys/sales/SalesSystemApplication.java
@@ -4,22 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 @ComponentScan({"com.sys.controller"})
 
 @SpringBootApplication
 @RestController
 public class SalesSystemApplication {
 
-	private static final Logger LOGGER=LoggerFactory.getLogger(SalesSystemApplication.class);
-	
 	public static void main(String[] args) {
 		SpringApplication.run(SalesSystemApplication.class, args);
-		
-		LOGGER.info("Info level log message");
-        LOGGER.debug("Debug level log message");
-        LOGGER.error("Error level log message");
+	
 	}
 
 }
